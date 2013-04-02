81577f2e78cd1945328903f2ec94f68ae24a516a
He Chunhui <hchunhui@mail.ustc.edu.cn>
2013-04-02 19:26:10 +0800

    [equal] 修复某些情况下证明项错误

--- src/dpll/equal_proof.c
+++ src/dpll/equal_proof.c
@@ -6,14 +6,7 @@
 #include "proof_utils.h"
 static int fa[64];
 static int id[64];
-static int vec[128];
-
-static int get_fa(int v)
-{
-	while(fa[v] != v)
-		v = fa[v];
-	return v;
-}
+static int count;
 
 static int get_fa_proof(int v)
 {
@@ -46,6 +39,7 @@ static void set_init()
 		fa[i] = i;
 		id[i] = 0;
 	}
+	count = 0;
 }
 
 static int set_find(int idx, int idy)
@@ -62,18 +56,23 @@ static int set_find(int idx, int idy)
 static void set_union(int idx, int idy, int hseq)
 {
 	int fx, fy;
-	fx = get_fa(idx);
-	fy = get_fa(idy);
-	if(fy != fx) {
-		fa[fx]=fy;
-		id[fx] = hseq;
-	}
+	fprintf(pout, "let H%d := ", count++);
+	fprintf(pout, "(trans_eq (trans_eq (sym_eq (");
+	fx = get_fa_proof(idx);
+	fprintf(pout, ")) H%d) ", hseq);
+	fy = get_fa_proof(idy);
+	fprintf(pout, ")");
+	fa[fx]= fy;
+	id[fx] = count-1;
+	fprintf(pout, " in ");
 }
 
 void equal_proof(int seq, struct lit_set *lit)
 {
 	int i, n;
 	struct pred p;
+	int vec[128];
+
 	fprintf(pout, "Definition L%d := ", seq);
 	n = bit2vec(lit->cp, lit->cn, vec);
 	set_init();
@@ -82,10 +81,10 @@ void equal_proof(int seq, struct lit_set *lit)
 		if(vec[i] < 0)
 		{
 			pred_get(&p, -vec[i]-1);
-			set_union(p.lv, p.rv, i);
-			fprintf(pout, "horn _ _ (fun H%d:(", i);
+			fprintf(pout, "horn _ _ (fun H%d:(", count++);
 			print_lit(-vec[i]);
 			fprintf(pout, ") => ");
+			set_union(p.lv, p.rv, count-1);
 		}
 	}
 
